e9d97c11afe175704dc82b015d699ebfe4990944
unknown <vasanthgamer2000@gmail.com>
2023-07-13 14:21:24 +0530

    Positive or negative and odd or even of numbers

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Enter the number");
+            number = int.Parse(Console.ReadLine());
+
+            if (number > 0) 
+            {
+                Console.WriteLine("The number is positive");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine("The number is negative");
+            }
+            else 
+            {
+                Console.WriteLine("The number is zero");
+            }
+
+           
+            string evenOrodd = ( number % 2 ==0 ) ? "even" : "odd";
+
+            Console.WriteLine("The number is " +  evenOrodd);
+            Console.ReadKey();
+        }
+    }
+}
